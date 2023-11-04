e28ff2ae0739964e830ae3c49912f0b2dcdb6c26
231051TertiusJ <125471734+231051TertiusJ@users.noreply.github.com>
2023-11-04 11:10:37 +0200

    add basic form validation

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    // ---------------------------------------------------------------------------------------------------
+    // Sign up form
+    // ---------------------------------------------------------------------------------------------------
+
+    // ---------------------------------------------------------------------------------------------------
+    // On Submit, prevent the default form submission
+
+    $('#signupForm').submit(function(event) {
+
+        event.preventDefault();
+        
+        if (this.checkValidity() === false) {
+            event.stopPropagation();
+        } else {
+            // Add any submission code here, like saving the data to localStorage
+            window.location.href = 'pages/browse.html';
+        }
+        $(this).addClass('was-validated');
+    });
+});
