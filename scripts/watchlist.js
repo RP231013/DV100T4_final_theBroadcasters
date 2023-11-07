0e38af4bc1a1d5a3868e7f9193bd581f9d5a595f
RP231013 <231013@virtualwindow.co.za>
2023-11-07 17:25:57 +0200

    Updated remove method
    Remove item from local storage.
    
    Co-Authored-By: 231051TertiusJ <125471734+231051TertiusJ@users.noreply.github.com>

--- scripts/watchlist.js
+++ scripts/watchlist.js
@@ -1,26 +1,28 @@
-$(document).ready(function() {
-    const watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
+//global watchlist array with movie IDs
+let watchlistArray = JSON.parse(localStorage.getItem('watchlist')) || [];
 
-    if (watchlist.length === 0) {
-        // Display a message or perform any action when the watchlist is empty
+$(document).ready(function() {
+
+
+    if (watchlistArray.length === 0) {
+        // Display a message when the watchlist is empty
         $('#watchListMovies').html('<p>Your watchlist is empty.</p>');
         return;
     }
-
-    loadMovieCards(watchlist);
-
-
+    //load the watchlist
+    loadMovieCards(watchlistArray);
 });
 
 
-
-function loadMovieCards(watchlist){
+//function for appending movie cards to page
+function loadMovieCards(watchlistArray){
+    // clears container
     $('#watchListMovies').empty();
 
-    
+    // add each element
+    watchlistArray.forEach(idElem => {
 
-    watchlist.forEach(idElem => {
-
+        //gets movie data from api using ID stored in local storage
         const apiURL = `https://api.themoviedb.org/3/movie/${idElem}?api_key=2ac1e5ad6ec723f6618988e193d2939a`;
         
         $.ajax({
@@ -46,7 +48,7 @@ function loadMovieCards(watchlist){
                                     <p class="card-text mt-4" id="overview">${movie.overview}</p>
                                     <div class="card-button mt-5">
                                         <a href="#" class="btn btn-primary">Watch</a>
-                                        <a href="#" class="btn btn-primary" id="likeBTN">
+                                        <a href="#" class="btn btn-primary remove-btn" data-movie-id="${idElem}"> 
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                                             </svg>
@@ -59,9 +61,8 @@ function loadMovieCards(watchlist){
                 </div>
             `;
 
-            // Append the new card to the watchlist container
+            // Append card to the watchlist container
             $('#watchListMovies').append(movieCard);
-
     
             }, 
             error: function(error){
@@ -70,16 +71,26 @@ function loadMovieCards(watchlist){
         })
 
     });
-
-    
-
-    
 }
 
-// Remove item from local storage
 
-$(document).on('click', '#likeBTN' , function() {
-    $(this).parents(".card").remove();
-    localStorage.removeItem('card');
+//method for removing movie from local storage 
+$(document).on('click', '.remove-btn', function() {
+    // Retrieve the movie ID stored in the data attribute of the clicked button
+    const movieIdToRemove = $(this).data('movie-id');
 
-})
+    // Find the index of this movie ID in the watchlist array
+    const indexToRemove = watchlistArray.indexOf(movieIdToRemove);
+
+    // If the movie is found in the watchlist array
+    if (indexToRemove !== -1) {
+        // Remove the movie from the array
+        watchlistArray.splice(indexToRemove, 1);
+
+        // Update the local storage with the new watchlist array
+        localStorage.setItem('watchlist', JSON.stringify(watchlistArray));
+
+        // Reload the movie cards to show changes
+        loadMovieCards(watchlistArray);
+    }
+});
